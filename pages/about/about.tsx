04afd62c1eb7e8fabdf9c5fb9180530a5642888a
MAHMOOD UL ISLAM <mahmood.islam@gmail.com>
2022-04-22 16:13:55 +0600

    Update about.tsx

--- pages/about/about.tsx
+++ pages/about/about.tsx
@@ -29,29 +29,29 @@ export default function VAbout() {
           <Fade>
             <h2>
               {useTypedText(`An energetic and imaginative Full-stack developer along with the ability to create web & mobile applications of all standards. High awareness of industry issues and trends; particularly regarding accessibility, usability, emerging technologies, and open-source CMS.
-	Full-stack web development:
-•	Identifying technical solutions
-•	Client facing skills
-•	Troubleshooting
-•	Web graphics
-•	Bug fixing
-•	Building UI & UX
-•	Maintaining data integrity
-•	Building functionality
-•	Web application development
-•	Mobile application development
+                              	Full-stack web development:
+                              •	Identifying technical solutions
+                              •	Client facing skills
+                              •	Troubleshooting
+                              •	Web graphics
+                              •	Bug fixing
+                              •	Building UI & UX
+                              •	Maintaining data integrity
+                              •	Building functionality
+                              •	Web application development
+                              •	Mobile application development
 
-	Experience:
-•HTML, CSS & JavaScript
-•React js, Next js, Redux, Node, Express
-•Mongo-DB & Firebase
-•React-Native, 
-•Docker, Kubernetes
-•Sass, Bootstrap, React-bootstrap, Material-UI, Ant-Design
-•Search Engine Optimisation
+                              	Experience:
+                              •HTML, CSS, JavaScript & Typescript
+                              •React js, Next js, Redux, Node, Express
+                              •Mongo-DB & Firebase
+                              •React-Native, 
+                              •Docker
+                              •Sass, Bootstrap, React-bootstrap, Material-UI, Ant-Design
+                              •Search Engine Optimization
 
-Responsible for all the works that belong to the web development from start to finish.
-`, 30, 350)}
+                              Responsible for all the works that belong to the web development from start to finish.
+                              `, 30, 350)}
             </h2>
 
           </Fade>
